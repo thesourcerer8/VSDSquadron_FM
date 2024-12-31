9dcb267102981146d5485a9f7e1b5cd2ac6c8ada
Philipp Gühring <pg@futureware.at>
2024-12-31 13:47:06 +0100

    Trying to make sure that the address itself is stable already and doesnt change in the same cycle

--- ram_infer/top.sv
+++ ram_infer/top.sv
@@ -10,8 +10,8 @@ module top(hw_clk,io0);
 
 	reg [16:0] counter=0;
 
-	reg [7:0] page_param [255:0] /* ram_style = "block" */ /* synthesis syn_ramstyle = "no_rw_check" */;
-	//initial page_param[0] <= 255;  // I was told that yosys wants this,
+	reg [7:0] page_param [255:0] /* synthesis ram_style = "block" */ /* synthesis syn_ramstyle = "no_rw_check" */;
+	initial page_param[0] <= 255;  // I was told that yosys wants this,
 	//but it doesnt seem to work
 	reg [7:0] io_rd_data_out = 14;
 	reg [7:0] data_out;
@@ -23,8 +23,11 @@ module top(hw_clk,io0);
 always @(posedge hw_clk) begin
 
 	counter<=counter+1;
-	page_idx<=3;
-
+	if(counter==0)
+	begin
+    		page_idx<=3;
+	end
+	else
 	if(counter==1)
 	begin
 		page_param[page_idx]	<= io_rd_data_out[7:0];
