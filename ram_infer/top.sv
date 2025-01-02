539513d454b53a5d12870e1845ecb3076ebfdbba
Philipp Gühring <pg@futureware.at>
2025-01-02 18:33:36 +0100

    Finally it infers the BRAM!

--- ram_infer/top.sv
+++ ram_infer/top.sv
@@ -10,7 +10,7 @@ module top(hw_clk,io0);
 
 	reg [16:0] counter=0;
 
-	reg [7:0] page_param [255:0] /* synthesis ram_style = "block" */ /* synthesis syn_ramstyle = "no_rw_check" */;
+	reg [7:0] page_param [255:0] /* synthesis syn_ram_style = "block" */ /* synthesis syn_ramstyle = "no_rw_check" */;
 	initial page_param[0] <= 255;  // I was told that yosys wants this,
 	//but it doesnt seem to work
 	reg [7:0] io_rd_data_out = 14;
@@ -26,21 +26,26 @@ always @(posedge hw_clk) begin
 	if(counter==0)
 	begin
     		page_idx<=3;
+		data_out<=0;
 	end
-	else
-	if(counter==1)
+	else if(counter==1)
 	begin
 		page_param[page_idx]	<= io_rd_data_out[7:0];
 	end
         else if (counter==2)
 	begin
 		data_out <= page_param[page_idx];
+		io_rd_data_out <= io_rd_data_out +1;
 	end
 	else if (counter==3)
 	begin
-		page_param[page_idx]	<= io_rd_data_out[7:0]+1;
+		page_idx<=page_idx+1;
 	end
-        else if (counter==4)
+	else if (counter==4)
+	begin
+		page_param[page_idx]	<= io_rd_data_out[7:0];
+	end
+        else if (counter==5)
 	begin
 		data_out <= page_param[page_idx];
 	end
